b7384e1a12700fa4bc3b446ad3be621acffa9120
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-18 21:20:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,11 +10,15 @@ class Author
   end
 
   def add_post(post)
-    post.author == self
+    post.author = self
   end
 
   def add_post_by_title(title)
     new_post = Post.new(title)
     new_post.author = self
+  end
+
+  def self.post_count
+    Post.all.count {|post| post.author == self}
   end
 end
