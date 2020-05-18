ad3e2d1c7ab308e74765d46eb8770151fca885af
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-18 21:17:17 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,20 @@
 class Author
-  attr_accessor :name, :posts
+  attr_accessor :name
 
   def initialize(name)
     @name = name
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def add_post(post)
+    post.author == self
+  end
+
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self
   end
 end
